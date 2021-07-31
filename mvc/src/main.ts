cdf3322f6b09de879a3f8a5401b10b90f8265e5a
adonistseriotis <adonis.tseriotis@gmail.com>
2021-07-31 21:05:39 +0300

    Some final touches

--- mvc/src/main.ts
+++ mvc/src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   app.use(cookieParser())
   app.get(NextModule).prepare().then(() => {
     
-    app.listen(8001, 'localhost', () => {
-      console.log('> Ready on http://localhost:8001 with Next.js!');
+    app.listen(8001, '0.0.0.0', () => {
+      console.log('> Ready on http://0.0.0.0:8001 with Next.js!');
     });
   });
 }
